d08eeb377b1fdd8d02b9cfe09105a894fc634471
hrafnkell <hrafnkell@no.home.com>
2018-01-14 15:03:18 +0000

    First version

---
+++ PostcodeLibrary/Postcode.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PostcodeLibrary
+{
+    public class Postcode
+    {
+        private static string _pattern = 
+            "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$";
+
+        public bool Test(string postcode)
+        {
+            if(string.IsNullOrEmpty(postcode)) return false;
+
+            Regex regex = new Regex(_pattern);
+            Match match = regex.Match(postcode);
+            return match.Success;
+        }
+    }
+}
---
+++ PostcodeLibraryTests/PostcodeTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using PostcodeLibrary;
+
+namespace PostcodeLibraryTests
+{
+    [TestFixture]
+    public class PostcodeTests
+    {
+        private Postcode _postcode;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _postcode = new Postcode();
+        }
+
+        [Test]
+        public void Postcode_Is_Created()
+        {
+            Assert.IsNotNull(_postcode);
+        }
+
+        [TestCase("TKCA 1ZZ")]
+        [TestCase("ZZ99 3CZ")]
+        [TestCase("TW8 9GS")]
+        [TestCase("S6 1SW")]
+        public void Valid_Postcode_Under_Test_Passes(string postcode)
+        {
+            bool result = _postcode.Test(postcode);
+            Assert.True(result);
+        }
+
+        [TestCase("")]
+        [TestCase("WIBBLE")]
+        [TestCase("A B C")]
+        [TestCase(null)]
+        public void Non_Valid_Postcode_Under_Test_Fails(string postcode)
+        {
+            bool result = _postcode.Test(postcode);
+            Assert.False(result);
+        }
+    }
+}
